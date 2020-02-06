7838a8a2d59a40e2a25e715cdbfbbc4e3b8f7879
Sergio Moya <smoya89@gmail.com>
2020-02-06 13:41:49 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/smoya/netlify-function-go-example
+
+go 1.13
+
+require github.com/aws/aws-lambda-go v1.13.3
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(func(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusOK,
+			Body:       "Hello World!",
+		}, nil
+	})
+}
