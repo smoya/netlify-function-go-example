e07dbd506cc074ccd7948b230efa823fc519b1d7
Sergio Moya <smoya89@gmail.com>
2020-02-14 02:25:12 +0000

    file and function

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	lambda.Start(func(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusOK,
-			Body:       "Hello World",
+			Body:       "Hello my World",
 		}, nil
 	})
 }
